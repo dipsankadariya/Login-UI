b13beff23e426be5d767b4d6976bde401d5c6f2e
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-06-20 10:40:33 +0545

    new updates

---
+++ login/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Login User Interface",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("LogIn User Interface"),
+      ),
+      body: Column(
+        children: [Text("Log in"), Column()],
+      ),
+    );
+  }
+}
